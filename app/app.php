c1a6e972d1240821ba437e1f07f02b98949eb00b
Julio Diaz & Chris Lytsell <student@epicodus.com>
2015-08-11 11:34:08 -0700

    Write rest of app and fixed hom_form

--- app/app.php
+++ app/app.php
@@ -12,5 +12,13 @@
 		return $app['twig']->render('home_form.html.twig');
 	});
 	
+	$app->get("/view_anagram", function() use ($app) {
+		$anagram_check = new Anagram;
+		$word = $_GET['word'];
+		$list_to_compare = explode(" ", $_GET['list']);
+		$list_of_anagrams = $anagram_check->checkAnagram($word, $list_to_compare);
+		return $app['twig']->render('list_of_anagrams.html.twig', array('result' => $list_of_anagrams));
+	});
 	
+	return $app;
 ?>
