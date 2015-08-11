47cb50e971f9dc69ec84ef134129f1d45ec02705
Julio Diaz & Chris Lytsell <student@epicodus.com>
2015-08-11 12:12:02 -0700

    Account for partial Anagrams

--- src/Anagram.php
+++ src/Anagram.php
@@ -15,6 +15,15 @@
 						if($main_word == $possible_word) {
 							array_push($confirmed_anagrams, $possible_anagram);
 						}
+					} else {
+						$main_word = str_split(strtolower($word));
+						$possible_word = str_split(strtolower($possible_anagram));
+						sort($main_word);
+						sort($possible_word);
+						if (array_intersect($main_word, $possible_word) == $main_word) {
+							array_push($confirmed_anagrams, $possible_anagram);
+						}
+						
 					}
 				}
 			}
