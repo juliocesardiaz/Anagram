051a4aaa7ebe4c0d32032f36ed0a2023f18d7abf
Julio Diaz & Chris Lytsell <student@epicodus.com>
2015-08-11 10:04:58 -0700

    First Commit

---
+++ tests/AnagramTest.php
@@ -0,0 +1,15 @@
+<?php 
+	require_once "src/Anagram.php";
+	
+	class AnagramTest extends PHPUnit_Framework_TestCase
+	{
+		function testSimpleAnagram()
+		{
+			//Arrange
+			$test = new Anagram;
+			$inputWord = "ear";
+			$inputList = array("dog", "bat", "era")
+			
+		}
+	}
+?>
