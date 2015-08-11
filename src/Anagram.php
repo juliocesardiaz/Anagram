2a27ab11a73f385753dcf1b0b19ee911e4224cd2
Julio Diaz & Chris Lytsell <student@epicodus.com>
2015-08-11 11:00:03 -0700

    Third Test Pass

--- src/Anagram.php
+++ src/Anagram.php
@@ -6,10 +6,20 @@
 			$confirmed_anagrams = array();
 			foreach ($listOfPossibleAnagrams as $possible_anagram)
 			{
-				if ($possible_anagram == $word)
-				{
-					array_push($confirmed_anagrams, "No anagrams");
+				if ($possible_anagram != $word) {
+					if(strlen($word) == strlen($possible_anagram)) {
+						$main_word = str_split(strtolower($word));
+						$possible_word = str_split(strtolower($possible_anagram));
+						sort($main_word);
+						sort($possible_word);
+						if($main_word == $possible_word) {
+							array_push($confirmed_anagrams, $possible_anagram);
+						}
+					}
 				}
+			}
+			if(empty($confirmed_anagrams)) {
+				array_push($confirmed_anagrams, "No anagrams");
 			}
 			return $confirmed_anagrams;
 		}
